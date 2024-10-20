a254cffac7051094c9dd7b51d40c4bab53d95f47
naimulhassan <developernaimul00@gmail.com>
2024-10-20 15:27:37 +0600

    improve code

---
+++ lib/theme/light_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_colors.dart';
+
+ThemeData themeData = ThemeData(
+
+    scaffoldBackgroundColor: AppColors.background,
+    appBarTheme: const AppBarTheme(backgroundColor: AppColors.background));
+
+
