f37e51203511cb687a3dfdba1ad5d8a2079d7b25
naimulhassan <developernaimul00@gmail.com>
2024-10-22 09:33:34 +0600

    payment done

--- android/app/src/main/kotlin/com/example/vapeless/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vapeless/MainActivity.kt
@@ -2,4 +2,7 @@ package com.example.vapeless
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity() {}
