f9653352721edfd0fe13541a6f1ee8ec6631df0c
naimulhassan <developernaimul00@gmail.com>
2024-10-26 09:24:47 +0600

    improve code

--- lib/controllers/payment/payment_controller.dart
+++ lib/controllers/payment/payment_controller.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
+// import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
 import 'package:vapeless/utils/app_utils.dart';
@@ -18,57 +18,57 @@ class PaymentController extends GetxController {
   static PaymentController get instance => Get.put(PaymentController());
 
   paymentPaypal() {
-    return PaypalCheckoutView(
-      sandboxMode: true,
-      clientId: clientId,
-      secretKey: secretKey,
-      transactions: const [
-        {
-          "amount": {
-            "total": "100",
-            "currency": "USD",
-            "details": {
-              "subtotal": "100",
-              "shipping": '0',
-              "shipping_discount": '0'
-            }
-          },
-          "description": "The payment transaction description.",
-          "item_list": {
-            "items": [
-              {
-                "name": "Apple",
-                "quantity": 1,
-                "price": "100",
-                "currency": "USD"
-              },
-            ],
-          }
-        }
-      ],
-      note: "Contact us for any questions on your order.",
-      onSuccess: (Map params) async {
-        Utils.snackBarMessage("Payment successful", "");
-        Navigator.pop(Get.context!);
-        if (kDebugMode) {
-          print("onSuccess: $params");
-        }
-      },
-      onError: (error) {
-        Navigator.pop(Get.context!);
-        Get.snackbar("Something went wrong,", "Try again!");
-        if (kDebugMode) {
-          print("onError: $error");
-        }
-      },
-      onCancel: () {
-        Navigator.pop(Get.context!);
-        Get.snackbar("Payment cancelled", "");
-        if (kDebugMode) {
-          print('cancelled:');
-        }
-      },
-    );
+    // return PaypalCheckoutView(
+    //   sandboxMode: true,
+    //   clientId: clientId,
+    //   secretKey: secretKey,
+    //   transactions: const [
+    //     {
+    //       "amount": {
+    //         "total": "100",
+    //         "currency": "USD",
+    //         "details": {
+    //           "subtotal": "100",
+    //           "shipping": '0',
+    //           "shipping_discount": '0'
+    //         }
+    //       },
+    //       "description": "The payment transaction description.",
+    //       "item_list": {
+    //         "items": [
+    //           {
+    //             "name": "Apple",
+    //             "quantity": 1,
+    //             "price": "100",
+    //             "currency": "USD"
+    //           },
+    //         ],
+    //       }
+    //     }
+    //   ],
+    //   note: "Contact us for any questions on your order.",
+    //   onSuccess: (Map params) async {
+    //     Utils.snackBarMessage("Payment successful", "");
+    //     Navigator.pop(Get.context!);
+    //     if (kDebugMode) {
+    //       print("onSuccess: $params");
+    //     }
+    //   },
+    //   onError: (error) {
+    //     Navigator.pop(Get.context!);
+    //     Get.snackbar("Something went wrong,", "Try again!");
+    //     if (kDebugMode) {
+    //       print("onError: $error");
+    //     }
+    //   },
+    //   onCancel: () {
+    //     Navigator.pop(Get.context!);
+    //     Get.snackbar("Payment cancelled", "");
+    //     if (kDebugMode) {
+    //       print('cancelled:');
+    //     }
+    //   },
+    // );
   }
 
   /// stripe payment
